0bef68262952977f87e61ba7774c529fa5b75f9a
史先生 <87529060+1718744251@users.noreply.github.com>
2021-07-20 14:45:50 +0100

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,95 +1,155 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void add(int aa);
+#define Max(X,Y)(X>Y?X:Y)
+#include<math.h>
 int main()
 {
-	int a;
-	printf("请输入：");
-	scanf_s("%d", &a);
-	add(a);
+	//int a, b, c;
+	//printf("请输入三个数:");
+	//scanf("%d%d%d", &a, &b, &c);
+	//int max1 = 0, max2 = 0, max3 = 0;
+	//if (a > b && a > c & b > c)
+	//{
+	//	printf("%d>%d>%d", a, b, c);
+	//}
+	//else if (a > b && a > c && c > b)
+	//{
+	//	printf("%d>%d>%d", a, c, b);
+	//}
+	//else if (b>a&&b>c&&a>c)
+	//{
+	//	printf("%d>%d>%d", b,a,c);
+	//}
+	//else if (b > a && b > c && c>a)
+	//{
+	//	printf("%d>%d>%d", b,c,a);
+	//}
+	//else if (c>a&&c>b&&a>b)
+	//{
+	//	printf("%d>%d>%d",c,a,b);
+	//}
+	//else if (c > a && c > b && b>a)
+	//{
+	//	printf("%d>%d>%d", c,b,a);
+	//}
+
+	//int a, b, c;
+	//printf("请输入三个数:");
+	//scanf("%d%d%d", &a, &b, &c);
+	//if (a < b)
+	//{
+	//	int temp = a;
+	//	a=b;
+	//	b=temp;
+	//}
+	//else if (a < c)
+	//{
+	//	int temp = a;
+	//	a = c;
+	//	c = temp;
+	//}
+	//else if (b<c)
+	//{
+	//	int temp = b;
+	//	b = c;
+	//	c = temp;
+	//}
+	//printf("%d>%d>%d", a, b, c);
+
+	//int i = 1;
+	//for (i = 1; i <= 100; i++)
+	//{
+	//	if (0 == i % 3)
+	//	{
+	//		printf("%d\t", i);
+	//		if (0 == i % 5)
+	//		{
+	//			printf("\n");
+	//		}
+	//	}
+	//}
+
+	//求最大公约数(辗转相除法)
+	//int m ;
+	//int n ;
+	//int r = 0;
+	//printf("请输入两个数:");
+	//scanf("%d%d", &m, &n);
+	//while (r=m % n)
+	//{
+	////	r = m % n;
+	//	m = n;
+	//	n = r;
+	//}
+	//printf("最大公约数是:%d", n);
+
+
+	//判断year是否为闰年
+	//1.能被4整除并且不能被100整除是闰年
+	//2.能被400整除是闰年
+	//int year=0;
+	//int count = 0;
+	//for (year = 1000; year <= 2000; year++)
+	//{
+	//	if (0 == year % 4 && 0 != year % 100|| 0 == year % 400)
+	//	{
+	//		printf("%d\t", year);
+	//		count++;
+	//		if (0 == count % 10)
+	//		{
+	//			printf("\n");
+	//		}
+	//	}
+	//}
+	//printf("count=%d\n", count);
+
+	//int i;
+	//for (i = 100; i <= 200; i++)
+	//{
+	//	//素数判断的规则
+	//	int n = 0;
+	//	for (n = 2; n < i ; n++)
+	//	{
+	//		if (i % n == 0)
+	//		{
+	//			break;
+	//		}
+	//	}
+	//	if (n == i)
+	//	{
+	//		printf("%d\t", i);
+	//	}
+	//}
+
+
+	
+	int i=0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		//素数判断的规则
+		int n = 0;
+		//for (n = 2; n < sqrt(i); n++)  //for(n=2;n<(i/2);n++)一样的
+		for(n=2;n<(i/2);n++)
+		{
+			if (i % n == 0)
+			{
+				break;
+			}
+		}
+		
+		if (n>sqrt(i))
+		{
+			printf("%d\t", i);
+			count++;
+			if (count % 5 == 0) 
+			{
+				printf("\n");
+			}
+			
+		}
+	}
+
+								
 	return 0;
 }
-void add(int aa)
-{
-	int i = 0, j = 0;
-	int arr1[7] = { 1,4,8,11,9,5,6 };
-	int arr2[6] = { 3,7,11,5,9,1 };
-	float m, n, p;
-	int max1, max2, max3;
-	max1 = 0, max2 = 0, max3 = 0;
-	float sum1, sum2, sum3;
-	sum1 = 0, sum2 = 0, sum3 = 0;
-	int ass[3][6] = { {2456,1847,1243,1600,2346,2757 },{3045,2018,1725,2020,2458,1436},{1427,1175,1046,1976,1477,2018} };
-	ass[i][j] = max1;
-	ass[i][j] = max2;
-	ass[i][j] = max3;
-	switch (aa)
-	{
-	case 1:
-
-		for (i = 0; i < 1; i++)
-		{
-			for (j = 0; j <= 5; j++)
-			{
-				sum1 += ass[i][j];
-				m = sum1 / 6;
-			}
-			printf("%.2f\n", m);
-		}
-		for (i = 1; i < 2; i++)
-		{
-			for (j = 0; j <= 5; j++)
-			{
-				sum2 += ass[i][j];
-				n = sum2 / 6;
-			}
-			printf("%.2f\n", n);
-		}
-		for (i = 2; i < 3; i++)
-		{
-			for (j = 0; j <= 5; j++)
-			{
-				sum3 += ass[i][j];
-				p = sum3 / 6;
-			}
-			printf("%.2f\n", p);
-		}
-		break;
-
-
-	case 2:
-		for (i = 0; i < 1; i++)
-			for (j = 0; j <= 5; j++)
-			{
-				if (ass[i][j] > max1)max1 = ass[i][j];
-			}
-		printf("max1=%d\n", max1);
-		for (i = 1; i < 2; i++)
-			for (j = 0; j <= 5; j++)
-			{
-				if (ass[i][j] > max2)max2 = ass[i][j];
-			}
-		printf("max2=%d\n", max2);
-		for (i = 2; i < 3; i++)
-			for (j = 0; j <= 5; j++)
-			{
-				if (ass[i][j] > max3)max3 = ass[i][j];
-			}
-		printf("max3=%d\n", max3);
-		break;
-
-	/*case 3:
-		printf("arr1:");
-		for (i = 0; i <= 6; i++)
-			printf("%d", arr1[6 - i]);
-		printf("\t");
-		printf("arr2:");
-		for (j = 0; j <= 5; j++)
-			printf("%d", arr2[5 - j]);
-		break;*/
-
-	default:
-		printf("不在范围内");
-		break;
-	}
-}
-	
