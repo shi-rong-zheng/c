e01e61c4a6022997332c79e6f4114bd00272d09d
史先生 <87529060+1718744251@users.noreply.github.com>
2021-07-27 22:13:53 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,83 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<time.h>
-#include"game.h"
 void menu()
 {
+	printf("******************  扫 雷  ***************\n");
 	printf("******************************************\n");
-	printf("******** 1.play           0.exit *********\n");
+	printf("*****************  1.play  ***************\n");
+	printf("*****************  0.exit  ***************\n");
 	printf("******************************************\n");
-}
 
+}
 void game()
 {
-	char ret = 0;
-	//数组-存放走出棋盘信息
-	char board[ROW][COL] = { 0 };//全部是空格
-	//初始化棋盘
-	InitBoard(board, ROW, COL);
-	//打印棋盘
-	DisplayBorad(board,ROW,COL);
-	//下棋
-	while (1)
-	{
-		//玩家下棋
-		BlayerMove(board,ROW,COL);
-		DisplayBorad(board, ROW, COL);
-		//判断玩家是否赢
-		ret = IsWin(board,ROW,COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-		//电脑下棋
-		ComputerMove(board, ROW, COL);
-		DisplayBorad(board, ROW, COL);
-		//判断电脑是否赢
-		ret = IsWin(board,ROW,COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-	}
-	if (ret == '*')
-	{
-		printf("玩家赢\n");
-	}
-	else if (ret == '#')
-	{
-		printf("电脑赢\n");
-	}
-	else
-	{
-		printf("平局\n");
-	}
+
 }
 void test()
 {
 	int input = 0;
-	srand((unsigned int)time(NULL));
+	menu();
 	do
 	{
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
+		printf("请选择(0或1):>");
+		scanf("%d",&input);
 		switch (input)
 		{
 		case 1:
 			game();
-			break;  
+			break;
 		case 0:
 			printf("退出游戏\n");
 			break;
 		default:
-			printf("选择错误，请重新选择\n");
-			break;
+			printf("输入错误，请重新输入");
 		}
 	} while (input);
+	
 }
 int main()
 {
 	test();
 	return 0;
 }
+ 
