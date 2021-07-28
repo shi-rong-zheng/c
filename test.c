f3c9753c2d7a4330689f2d1a31583143839bf331
史先生 <87529060+1718744251@users.noreply.github.com>
2021-07-28 22:17:11 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,26 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
+#include"game.h"
 void menu()
 {
-	printf("******************  扫 雷  ***************\n");
-	printf("******************************************\n");
-	printf("*****************  1.play  ***************\n");
-	printf("*****************  0.exit  ***************\n");
-	printf("******************************************\n");
-
+	printf("*************************************\n");
+	printf("**************1.play*****************\n");
+	printf("**************0.exit*****************\n");
+	printf("*************************************\n");
 }
 void game()
 {
-
+	//存放棋盘数组
+	char mine[ROWS][COLS] = { 0 };
+	char show[ROWS][COLS] = { 0 };
+	//初始化棋盘
+	IsitBoard(mine,ROWS,COLS, '0');
+	IsitBoard(show, ROWS, COLS, '*');
+	//打印棋盘
+	//DisplayBoard(mine,ROW,COL);
+	DisplayBoard(show, ROW, COL);
+	printf("\n");
+	//布置雷
+	SetMine(mine,ROW,COL);
+	DisplayBoard(mine, ROW, COL);
+	//扫雷
+	FindMine(mine,show,ROW,COL);
+	DisplayBoard(show, ROW, COL);
 }
 void test()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	menu();
 	do
 	{
-		printf("请选择(0或1):>");
-		scanf("%d",&input);
+		printf("请选择:>");
+		scanf("%d", &input);
 		switch (input)
 		{
 		case 1:
@@ -30,14 +44,12 @@ void test()
 			printf("退出游戏\n");
 			break;
 		default:
-			printf("输入错误，请重新输入");
+			printf("输入错误，请重新输入！！！\n");
 		}
 	} while (input);
-	
 }
 int main()
 {
 	test();
 	return 0;
 }
- 
