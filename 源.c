1ee4158b24f93b775d0eaf026c95e284cede9f7d
史先生 <87529060+shi-rong-zheng@users.noreply.github.com>
2021-08-21 19:53:34 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,10 +1,79 @@
+/*#include<stdio.h>
+int main()
+{
+	int arr1[] = { 1,4,8,11,9,5,6 };
+	int i;
+	for (i = 0; i <= 6; i++)
+{
+	printf("arr1[%d]",arr1[i]);
+}
+	for (i = 6; i >= 0; i--)
+	printf("arr1[%d]", arr1[i]);
+	return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+	int m[] = { 10,20,30,40,50 };
+	int i;
+	//printf(" the ordering of array:\n");
+	for (i = 0; i < 5; i++)
+	{
+		printf("the number of m[% d] = % d\n", i, m[i]);
+	}
+	//printf(" the ordering of array:\n");
+	for (i = 4; i >= 0; i--)
+	{
+		printf("the number of m[% d] = % d\n", i, m[i]);
+	}
+	return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+	int i, j;
+	int arr1[] = { 1,4,8,11,9,5,6 };
+	int arr2[] = { 3,7,11,5,9,1 };
+	{
+		for (i = 6; i >= 0; i--)
+			printf("arr1[%d]", arr1[i]);
+		printf("\t");
+	}
+	for (j = 5; j >= 0; j--)
+		printf("arr2[%d]", arr2[j]);
+}*/
+/*#include<stdio.h>
+int main()
+{
+	int i, j;
+	int teap;
+	int arr1[7] = { 1,4,8,11,9,5,6 };
+	int arr2[6] = { 3,7,11,5,9,1 };
+	printf("arr2:");
+	for (j = 5; j >= 0; j--)
+	printf("%d", arr2[j]);
+    printf("\t");
+	printf("arr1:");
+	for (i = 6; i >=0; i--)
+	printf("%d", arr1[i]);
+	return 0;
+}*/
+
 #include<stdio.h>
 int main()
 {
-    float a, b, c;
-    printf("请输入：");
-    scanf_s("%f %f %f", &a, &b, &c);
-    (a == b || a == c || b == c) ? printf("等腰三角形\n"): printf("不是等腰三角形\n");
-    (a + b > c && a + c > b && b + c > a) ? printf("是三角形\n"): printf("不是三角形");
-    (c*c=a*a+b*b ||  b*b=a*a+c*c || a * a=b*b+c*c) ? printf("是直角") : printf("不是直角");
+	int i = 0, j = 0;
+	int arr1[] = { 1,4,8,11,9,5,6 };
+	int arr2[] = { 3,7,11,5,9,1 };
+	printf("转置srr1：");
+	for (i = 0; i < 1; i++)
+	{
+		fof(j = 0; j < 7; j++)
+		{
+			printf("%d\t", arr1[6 - i]);
+			printf("转置srr2：");
+		}
+		for (j = 0; j <= 5; j++)
+			printf("%d\t", arr2[5 - j]);
+	}
 }
